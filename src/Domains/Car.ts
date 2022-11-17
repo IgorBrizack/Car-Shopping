79e01309b832545c481ae488a740c2ae024caf20
Igor Brizack <igor_brizack@hotmail.com>
2022-11-17 17:38:39 -0300

    feat: task 01 done

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,12 +1,12 @@
 import ICar from '../Interfaces/ICar';
 
-class Cars {
-  private id: string | undefined;
-  private model: string;
-  private year: number;
-  private color: string;
-  private status: boolean;
-  private buyValue: number;
+class Car {
+  protected id: string | undefined;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status: boolean;
+  protected buyValue: number;
   private doorsQty: number;
   private seatsQty: number;
 
@@ -32,61 +32,69 @@ class Cars {
     this.status = status || false;
   }
 
-  public setId(id: string) {
-    this.id = id;
-  }
+  // public setId(id: string) {
+  //   this.id = id;
+  // }
 
-  public getId() {
-    return this.id;
-  }
+  // public getId() {
+  //   return this.id;
+  // }
 
-  public setModel(model: string) {
-    this.model = model;
-  }
+  // public setModel(model: string) {
+  //   this.model = model;
+  // }
 
-  public getModel() {
-    return this.model;
-  }
+  // public getModel() {
+  //   return this.model;
+  // }
 
-  public setYear(year: number) {
-    this.year = year;
-  }
+  // public setYear(year: number) {
+  //   this.year = year;
+  // }
 
-  public getYear() {
-    return this.year;
-  }
+  // public getYear() {
+  //   return this.year;
+  // }
 
-  public setColor(color: string) {
-    this.color = color;
-  }
+  // public setColor(color: string) {
+  //   this.color = color;
+  // }
 
-  public getColor() {
-    return this.color;
-  }
+  // public getColor() {
+  //   return this.color;
+  // }
 
-  public setBuyValue(buyValue: number) {
-    this.buyValue = buyValue;
-  }
+  // public setBuyValue(buyValue: number) {
+  //   this.buyValue = buyValue;
+  // }
 
-  public getBuyValue() {
-    return this.buyValue;
-  }
+  // public getBuyValue() {
+  //   return this.buyValue;
+  // }
 
-  public setDoorsQty(doorsQty: number) {
-    this.doorsQty = doorsQty;
-  }
+  // public setDoorsQty(doorsQty: number) {
+  //   this.doorsQty = doorsQty;
+  // }
 
-  public getDoorsQty() {
-    return this.doorsQty;
-  }
+  // public getDoorsQty() {
+  //   return this.doorsQty;
+  // }
 
-  public setStatus(status: boolean) {
-    this.status = status;
-  }
+  // public setSeatsQty(seatsQty: number) {
+  //   this.seatsQty = seatsQty;
+  // }
 
-  public getStatus() {
-    return this.status;
-  }
+  // public getSeatsQty() {
+  //   return this.seatsQty;
+  // }
+
+  // public setStatus(status: boolean) {
+  //   this.status = status;
+  // }
+
+  // public getStatus() {
+  //   return this.status;
+  // }
 }
 
-export default Cars;
+export default Car;
--- src/Services/CarsService.ts
+++ src/Services/CarsService.ts
@@ -1,11 +1,21 @@
+import Car from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
 import CarODM from '../Models/CarODM';
 
 class CarsService {
+  private createCarDomain(car: ICar | null): Car | null {
+    if (car) {
+      return new Car(
+        car,
+      );
+    }
+    return null;
+  }
+
   public async insertNewCar(newCar: ICar) {
     const carODM = new CarODM();
-    const result = carODM.create(newCar);
-    return result
+    const result = await carODM.create(newCar);
+    return this.createCarDomain(result);
   }
 }
 

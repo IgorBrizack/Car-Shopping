8dfa57d380783a95a037f0798f2a37c146dadecc
Igor Brizack <igor_brizack@hotmail.com>
2022-11-18 09:28:21 -0300

    teask 06 done

--- tests/unit/Services/carService.test.ts
+++ tests/unit/Services/carService.test.ts
@@ -55,4 +55,45 @@ describe('Testando a rota de Cars', () => {
 
     sinon.restore();
   });
+
+  it('Deve retornar todos os carros cadastrados', async function () {
+    const allCars: Car[] = [new Car(
+      {
+        id: '6376ea91b337b952bb80bae1',
+        model: 'marea',
+        year: 2002,
+        color: 'Pink',
+        buyValue: 15.99,
+        status: true,
+        doorsQty: 4,
+        seatsQty: 5,
+      },
+    ), new Car({
+      id: '6376eaa3b337b952bb80bae3',
+      model: 'escort',
+      year: 2005,
+      color: 'Preto',
+      buyValue: 15.99,
+      status: true,
+      doorsQty: 4,
+      seatsQty: 5,
+    })];
+    sinon.stub(Model, 'find').resolves(allCars);
+
+    const service = new CarService();
+    const result = await service.getAllCars();
+
+    expect(result).to.be.deep.equal(allCars);
+  });
+
+  it('Deve lançar uma exceção caso um seja passado um id inválido', async function () {
+    const invalidId = '12345678aaaaaaaaaaaaaaaaaaaa';
+
+    try {
+      const service = new CarService();
+      await service.idVerify(invalidId);
+    } catch (error) {
+      expect((error as Error).message).to.be.equal('Invalid mongo id');
+    }
+  });
 });
---
+++ tests/unit/Services/motorcycleService.test.ts
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import { describe } from 'mocha';
+import { Model } from 'mongoose';
+import sinon from 'sinon';
+import Motorcycle from '../../../src/Domains/Motorcycle';
+import MotorcycleService from '../../../src/Services/MotocyleService';
+
+describe('Testando a rota de Motorcycles', () => {
+  it('cadastrando uma nova motorcycle com sucesso', async function () {
+    const motorcycleOutput: Motorcycle = new Motorcycle({
+      id: '6376c07a3fe0c9fdb2d99ab8',
+      model: 'Honda Cb 600f Hornet',
+      year: 2005,
+      color: 'Yellow',
+      status: true,
+      buyValue: 30.000,
+      category: 'Street',
+      engineCapacity: 600,
+    });
+
+    sinon.stub(Model, 'create').resolves(motorcycleOutput);
+    
+    const service = new MotorcycleService();
+    const result = await service.insertNewMotocycle({ 
+      model: 'Honda Cb 600f Hornet',
+      year: 2005,
+      color: 'Yellow',
+      status: true,
+      buyValue: 30.000,
+      category: 'Street',
+      engineCapacity: 600 });
+
+    expect(result).to.be.deep.equal(motorcycleOutput);
+
+    sinon.restore();
+  });
+});
